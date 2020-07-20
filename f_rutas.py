952ba22917af25e98093c0951cb45aab52969d60
marianaplazas <marianaplazas@live.com>
2020-07-20 03:13:31 +0000

    [delete] functions and add the functional version

--- f_rutas.py
+++ f_rutas.py
@@ -1,29 +1,6 @@
 #!/usr/bin/python3
 import pdb
 
-def cmpr(matrix, start, dest):
-	weight = []
-	for a in range(len(matrix)):
-		if (matrix[a].index(start) == 0 and matrix[a].index(dest) == 1):
-			weight.append(matrix[i][2])
-	final = sum(weight)
-	return(finals)
-
-def f_weight(s_r, matrix):
-	for i in range(len(s_r)):
-		start = s_r[i]
-		if (i + 1 < len(s_r)):
-			dest = s_r[i + 1]
-		weight = cmpr(matrix, start, dest)
-		print(weight)
-
-def order_r(all_r, matrix):
-	weights = []
-	copy_all = all_r
-	for i in range(len(all_r)):
-		s_r = copy_all.pop(i)
-		f_weight(s_r, matrix)
-
 def rutas(inicio, dest, matrix, ruta, guardar_l, weight):
 
 	tmp = []
@@ -35,9 +12,12 @@ def rutas(inicio, dest, matrix, ruta, guardar_l, weight):
 	for i in range(len(tmp)):
 		inicio = tmp[i][1]
 		ruta.append(inicio)
+		weight += int(tmp[i][2])
 		result = rutas(inicio, dest, matrix, ruta, guardar_l, weight)
 		if (result[-1] == dest):
 			guardar_l.append(result[:])
+			guardar_l[-1].append(weight)
+		weight -= int(tmp[i][2])
 		ruta.pop()
 	if (len(ruta) == 1):
 		return guardar_l
@@ -54,4 +34,12 @@ for i in range(len(lists)):
 weight = 0
 
 lista_rutas = rutas(ruta_f[0], ruta_f[1], matrix, [ruta_f[0]], [], weight)
-order_r(lista_rutas, matrix)
+mini = 100000
+for i in range(len(lista_rutas)):
+	if (lista_rutas[i][-1] < mini):
+		mini = lista_rutas[i][-1]
+		
+for i in range(len(lista_rutas)):
+	if mini in lista_rutas[i]:
+		lista_rutas[i].pop()
+		print(lista_rutas[i])
