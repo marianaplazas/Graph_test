36bd2d4292ac65f9529b4a53c893712fb3fdc745
marianaplazas <marianaplazas@live.com>
2020-07-20 01:37:37 +0000

    [feature] cmpr function to finde weight not working

--- f_rutas.py
+++ f_rutas.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
 import pdb
 
-def rutas(inicio, dest, matrix, ruta, guardar_l):
+def cmpr(matrix, start, dest):
+	weight = []
+	for a in range(len(matrix)):
+		if (matrix[a].index(start) == 0 and matrix[a].index(dest) == 1):
+			weight.append(matrix[i][2])
+	final = sum(weight)
+	return(finals)
+
+def f_weight(s_r, matrix):
+	for i in range(len(s_r)):
+		start = s_r[i]
+		if (i + 1 < len(s_r)):
+			dest = s_r[i + 1]
+		weight = cmpr(matrix, start, dest)
+		print(weight)
+
+def order_r(all_r, matrix):
+	weights = []
+	copy_all = all_r
+	for i in range(len(all_r)):
+		s_r = copy_all.pop(i)
+		f_weight(s_r, matrix)
+
+def rutas(inicio, dest, matrix, ruta, guardar_l, weight):
 
 	tmp = []
 
@@ -12,7 +35,7 @@ def rutas(inicio, dest, matrix, ruta, guardar_l):
 	for i in range(len(tmp)):
 		inicio = tmp[i][1]
 		ruta.append(inicio)
-		result = rutas(inicio, dest, matrix, ruta, guardar_l)
+		result = rutas(inicio, dest, matrix, ruta, guardar_l, weight)
 		if (result[-1] == dest):
 			guardar_l.append(result[:])
 		ruta.pop()
@@ -28,6 +51,7 @@ ruta_f = ruta_f.split(",")
 matrix = []
 for i in range(len(lists)):
 	matrix.append(lists[i].split(","))
+weight = 0
 
-print(rutas(ruta_f[0], ruta_f[1], matrix, [ruta_f[0]], []))
-
+lista_rutas = rutas(ruta_f[0], ruta_f[1], matrix, [ruta_f[0]], [], weight)
+order_r(lista_rutas, matrix)
